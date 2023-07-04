4d1fc079f6405f9f864ec5000e0f8789ee74fb83
Shahrukh-source <sk1109697@gmail.com>
2023-07-04 00:13:22 -0700

    first commit

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import "./style.scss";
 
-import useFetch from "../../hooks/useFetch";
+import UseFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
 import VideosSection from "./videosSection/VideosSection";
@@ -11,8 +11,8 @@ import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
     const { mediaType, id } = useParams();
-    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-    const { data: credits, loading: creditsLoading } = useFetch(
+    const { data, loading } = UseFetch(`/${mediaType}/${id}/videos`);
+    const { data: credits, loading: creditsLoading } = UseFetch(
         `/${mediaType}/${id}/credits`
     );
 
